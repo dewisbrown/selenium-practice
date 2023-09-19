10536d1973d7cb5a0b38da188f48eadff0fa74a0
dewisbrown <luismoreno2103@gmail.com>
2023-09-18 19:51:59 -0500

    third successful test added

--- test_example_website.py
+++ test_example_website.py
@@ -19,3 +19,15 @@ def test_check_page_header():
     expected_text = 'Example Domain'
     assert expected_text in header_element.text, f'Expected text: {expected_text}, Actual text: {header_element.text}'
     driver.quit()
+
+
+# Test Case 3: Click a Link
+# https://www.selenium.dev/documentation/webdriver/elements/locators/
+def test_click_link():
+    driver = webdriver.Chrome()
+    driver.get('http://www.example.com')
+    link_element = driver.find_element(By.LINK_TEXT,'More information...')
+    link_element.click()
+    new_page_title = driver.title
+    assert new_page_title != 'Example Domain', 'Page title didn\'t change after clicking the link.'
+    driver.quit()
