65693ebd3bf1a4f7a944be8e0887502f843ab763
dewisbrown <luismoreno2103@gmail.com>
2023-09-18 20:22:05 -0500

    test case 4 added

--- test_example_website.py
+++ test_example_website.py
@@ -31,3 +31,20 @@ def test_click_link():
     new_page_title = driver.title
     assert new_page_title != 'Example Domain', 'Page title didn\'t change after clicking the link.'
     driver.quit()
+
+
+# Test Case 4: Submit a Form
+def test_submit_form():
+    driver = webdriver.Chrome()
+    driver.get('https://duckduckgo.com')
+    search_input = driver.find_element(By.NAME, 'q')
+    search_input.send_keys('Test query')
+
+    # In the Safari Web Inspector, right clicking an element allows you to copy the XPath
+    submit_button = driver.find_element(By.XPATH, '//*[@id="searchbox_homepage"]/div/div/button')
+    submit_button.click()
+
+    # Get result elements
+    search_results = driver.find_elements(By.TAG_NAME, 'h2')
+    assert len(search_results) > 0, f'No results found for "{search_input}"'
+    driver.quit()
